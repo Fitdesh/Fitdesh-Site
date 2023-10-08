e9e95b201cacfedef3af752c024ffbf79df5e96f
Fitdesh <87796905+Fitdesh@users.noreply.github.com>
2023-10-08 20:53:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ tampilkanTombolButton.addEventListener("click", function () {
         layer.style.display = "none";
     }
 });
+
+var targetDate = new Date("September 26, 2024 00:00:00").getTime();
+
+var countdown = setInterval(function() {
+    var now = new Date().getTime();
+
+    var timeRemaining = targetDate - now;
+
+    var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (timeRemaining < 0) {
+        clearInterval(countdown);
+        document.getElementById("countdown").innerHTML = "Domain Telah Berakhir!";
+    }
+}, 1000);
