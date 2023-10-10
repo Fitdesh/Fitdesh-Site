2ef6be274d00ea913d6b7a3ba904dd1f57707e49
Fitdesh <87796905+Fitdesh@users.noreply.github.com>
2023-10-10 20:01:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,22 +31,16 @@ var countdown = setInterval(function() {
 
 var teksTerlindungi = document.getElementById('teksTerlindungi');
 
-document.addEventListener('selectstart', function (e) {
-    if (e.target !== teksTerlindungi) {
+teksTerlindungi.addEventListener('selectstart', function(e) {
+    e.preventDefault();
+});
+
+teksTerlindungi.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+teksTerlindungi.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.key === 'a') {
         e.preventDefault();
     }
 });
-
-document.addEventListener('contextmenu', function (e) {
-    if (e.target !== teksTerlindungi) {
-        e.preventDefault();
-    }
-});
-
-document.addEventListener('keydown', function (e) {
-    var selectedText = window.getSelection().toString();
-    if (e.ctrlKey && e.key === 'a' && selectedText !== teksTerlindungi.innerText) {
-        e.preventDefault();
-    }
-});
-
